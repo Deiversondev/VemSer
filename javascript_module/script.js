f56014cb0b0bd9f2f27084b8186ce88526d7831d
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-10-24 12:08:47 -0300

    Reorganização de pastas e arquivos

---
+++ javascript_module/script.js
@@ -0,0 +1,22 @@
+/* 1) Crie um programa que, a partir de um texto que o usuário digitar (prompt), verifique:
+   - Se o texto for igual à 'SIM' informe um alerta para o usuário informando o texto "Parabéns";
+   - Se o texto for igual à 'Não' solicite novamente para o usuário informar algum texto e após isso mostre para ele o texto digitado em um alerta;
+   - Se for digitado qualquer outra string solicite uma confirmação para o usuário com o texto: 'Você tem noções dos seus atos?'
+
+
+2) Crie um programa que imprima no console os números de 1 até 10;
+
+
+
+3) Crie um programa que, através de laços de repetição, percorra uma determinada lista com os seguintes números: [1, 2, 7, 10, 18]
+   e imprima no console a soma de todos os elementos, neste caso seria 38;
+
+
+
+4) Crie um programa que mostre um prompt para o usuário pedindo para selecionar uma das opções: 1 - Continuar perguntando / 2 - Parar de perguntar
+   se ele digitar algo diferente de 1 ou de 2 deve mostrar um alerta para ele informando o erro e deve solicitar novamente que escolha uma opção.
+
+
+
+5) Crie um programa que atribua à uma varíavel e imprima no console ao final da execução os textos: 
+   'Sextou' ou 'Ainda não sextou' de acordo com a resposta do usuário, códigos 1 e 2 respectivamente; */
