ec7849ea54e22dd2f426e0337fce357c61f4f325
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-10-26 02:38:43 -0300

    Deleted files

--- javascript_module/homework/task_2.js
+++
@@ -1,4 +0,0 @@
-let user = prompt('Olá! Para realizarmos o seu atendimento, preciso que informe seu nome');
-let employee = prompt('Agora , informe o nome do atendente que agendou o seu atendimento');
-
-let message = alert(`Olá ${user} , me chamo ${employee}! Como posso ajudar?`);
