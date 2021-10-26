1b3f8d7647fb26d8744ef987ce4eb0393a221d90
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-10-26 12:45:02 -0300

    Adjustments made based on the Tutor's feedback

--- javascript_module/homework/homework_02/task_1.js
+++ javascript_module/homework/homework_02/task_1.js
@@ -1,11 +1,13 @@
-let years = [2018,2019,2020,2021];
+let years = [2017,2018,2019,2020,2021];
 let wage = 1000;
 let percentage = 1.5 ;
 
 
-for(let i = 1; i <= years.length ; i++){
+for(let i = 0; i < years.length ; i++){
+    console.log(`\nYear: ${years[i]}`);
+    console.log(`Percentage: ${percentage}`);
+    console.log(`Wage: ${( wage/100 * percentage) + wage}`);
+    console.log(`On year ${years[i]  } , the employee's wage is ${( wage/100 * percentage) + wage}`)
     percentage = percentage * 2;
+    
 }
-
-let totalWage =( wage/100 * percentage) + wage;
-console.log(`In 2021, the employee's wage will be ${totalWage}`)
