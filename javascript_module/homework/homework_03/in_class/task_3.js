9b909dcb1e7450464b7d9e235058301be261f0da
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-10-27 00:56:22 -0300

    Made some ajustments on the code

--- javascript_module/homework/homework_03/in_class/task_3.js
+++ javascript_module/homework/homework_03/in_class/task_3.js
@@ -1,21 +1,17 @@
+function erro(){
+    console.error('What you typed is not a number')
+}
 let n1 = parseInt(prompt('Insert first number'));
+let n2 = parseInt(prompt('Insert second number'));
 
-if (!isNaN(n1)) {
-    let n2 = parseInt(prompt('Insert second number'));
-
-if (!isNaN(n2)) {
-    function sum (num1,num2){
+function sum (num1,num2,err){
+    if (!isNaN(num1) && !isNaN(num2)) {
         let totalSum = num1 + num2;
         console.log(`The total is ${totalSum}`);
     }
-    sum(n1,n2)
-}
-else {
-    console.log('In order for the operation to carry on , both numbers need to be valid');
-}
-
+    else {
+        err();
+    }
 }
 
-else{
-    console.log('Please, insert valid numbers');
-}
+sum(n1,n2,erro)
