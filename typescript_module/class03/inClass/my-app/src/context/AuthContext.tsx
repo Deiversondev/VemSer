2965ef792e9a48990c7f75184abdf96caba1e193
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-11-26 00:19:15 -0300

    Refazendo upload do projeto

--- typescript_module/class03/inClass/my-app/src/context/AuthContext.tsx
+++ typescript_module/class03/inClass/my-app/src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import { createContext } from 'react'
 import api from '../api'
+import { useNavigate } from 'react-router'
 
 const AuthContext = createContext({})
 
@@ -11,21 +12,50 @@ interface LoginDTO {
 
 const AuthProvider: React.FC<any> = ({children}) => {
 
-        useEffect(() =>{
+    const [auth,setAuth] = useState<boolean>(false);
+    const [loading,setLoading] = useState<boolean>(true);
+    const navigate = useNavigate()
 
+
+        useEffect(() =>{
+            const token = localStorage.getItem('token');
+            if(token){
+                api.defaults.headers.common['Authorization'] = token;
+                setAuth(true)
+            }
+            setLoading(false)
+            
         },[])
+
         const handleLogin = async (user:LoginDTO) =>{
             const {data} = await api.post('/auth', user)
             console.log(data)
+            localStorage.setItem('token', data)
+            api.defaults.headers.common['Authorization'] = data;
+            navigate('/people')
+            // window.location.href = '/people';
+            setAuth(true);
         }
 
-    const [auth,setAuth] = useState(false);
+        const handleLogout = () => {
+            localStorage.removeItem('token');
+            api.defaults.headers.common['Authorization'] = '';
+            window.location.href = '/login';
+            setAuth(false);
+        }
+
+        if(loading){
+            return(
+                <h1>Test</h1>
+            )
+        }
 
     return (
-       <AuthContext.Provider value={{auth,handleLogin}}>
+       <AuthContext.Provider value={{auth,handleLogin, handleLogout,setAuth}}>
            {children}
        </AuthContext.Provider>
     )
 }
 
 export {AuthProvider, AuthContext}
+
--- typescript_module/class03/inClass/my-app/src/pages/People.tsx
+++ typescript_module/class03/inClass/my-app/src/pages/People.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useContext, useEffect } from 'react';
+import api from '../api';
+import axios from 'axios';
+import { AuthContext } from '../context/AuthContext';
 
 const People = () => {
+
+    const {setAuth} = useContext<any>(AuthContext)
+
+    useEffect(() => {
+        setAuth(true)
+    },[])
+
+    useEffect(() =>{
+        (async()=>{
+            const {data} = await api.get('/pessoa');
+            console.log(data);
+        })();
+    },[])
+
     return (
         <div>
             <h1>Hi there people</h1>
